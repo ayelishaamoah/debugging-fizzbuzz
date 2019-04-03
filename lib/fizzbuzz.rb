649223318a1e55613c3796daeb5973fd9d9c30be
Sophie Gill <sophie@makersacademy.com>
2019-04-03 11:38:48 +0100

    fizzbuzz game will now return a list of numbers

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,11 @@
 class FizzBuzz
+  def play(number)
+    result = []
+    start = 1
+    while start <= number
+      result << start
+      start += 1
+    end
+    return result.join("\n")
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,13 @@
 require 'fizzbuzz'
 
 describe FizzBuzz do
-  
+  describe "#play" do
+    it "returns 1 when 1 is given" do
+      expect(subject.play(1)).to eq "1"
+    end
+
+    it "returns 1, 2 when 2 is entered" do
+      expect(subject.play(2)).to eq "1\n2"
+    end
+  end
 end
