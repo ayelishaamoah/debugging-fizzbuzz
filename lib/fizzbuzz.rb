bd356fba03a25a97db47214d7ec01b81144c4bcf
Sophie Gill <sophie@makersacademy.com>
2019-04-03 11:44:43 +0100

    fizzbuzz now returns fizz and buzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,7 +3,13 @@ class FizzBuzz
     result = []
     start = 1
     while start <= number
-      result << start
+      if start % 3 == 0
+        result << "Fizz"
+      elsif start % 5 == 0
+        result << "Buzz"
+      else
+        result << start
+      end
       start += 1
     end
     return result.join("\n")
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,8 +6,16 @@ describe FizzBuzz do
       expect(subject.play(1)).to eq "1"
     end
 
-    it "returns 1, 2 when 2 is entered" do
+    it "returns a list of numbers" do
       expect(subject.play(2)).to eq "1\n2"
+    end
+
+    it "returns Fizz when a number is divisible by 3" do
+      expect(subject.play(3)).to eq "1\n2\nFizz"
+    end
+
+    it "returns Buzz when a number is divisible by 5" do
+      expect(subject.play(5)).to eq "1\n2\nFizz\n4\nBuzz"
     end
   end
 end
